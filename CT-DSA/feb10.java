bb6567f859e1e7323f9a7fa41c658f8a98429f86
AyshaChauhan29 <ayshachauhan29@gmail.com>
2026-02-12 12:06:48 +0530

    Update feb10.java

--- CT-DSA/feb10.java
+++ CT-DSA/feb10.java
@@ -28,15 +28,12 @@ public class feb10 {
       // }
 
 
-
-
-
+  //fibonacci, tribonacci, prime no. code using functions
       public static int fib(int n){
       if(n==0 || n==1) return n;
       int a = 0;
       int b = 1;
       int sum = 0;
-      
       for(int i=2; i<=n; i++){
         sum = a+b;
         a = b;
@@ -90,3 +87,4 @@ public class feb10 {
         
     }
 }
+
